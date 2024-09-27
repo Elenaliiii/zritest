c0fc8b64a9c00f1b746b80bb93be7fec4c0042c6
elenaliiii <pishro.apk@gmail.com>
2024-09-27 16:12:56 +0330

    titile

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2329368699"
+    serviceWorkerVersion: "3335388054"
   }
 });
